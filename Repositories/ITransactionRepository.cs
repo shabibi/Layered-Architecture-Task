50f51324086937fcc134d855ec219224fdb374c0
Afra Alshabibi <a.shabibi20@gmail.com>
2024-11-25 14:59:10 +0400

    add TransactionRepository and ITransactionRepository

---
+++ Repositories/ITransactionRepository.cs
@@ -0,0 +1,13 @@
+﻿using ServicesLab1.Models;
+
+namespace ServicesLab1.Repositories
+{
+    public interface ITransactionRepository
+    {
+        void AddTransaction(Transaction transaction);
+        void DeleteTransaction(int transactionId);
+        IEnumerable<Transaction> GetAllTransactions();
+        Transaction GetTransactionById(int transactionID);
+        void UpdaTransaction(Transaction transaction);
+    }
+}
---
+++ Repositories/TransactionRepository.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServicesLab1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesLab1.Repositories
+{
+    public class TransactionRepository : ITransactionRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TransactionRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Transaction> GetAllTransactions()
+        {
+            return _context.Transactions.Include(t => t.transaction).ToList();
+        }
+        public Transaction GetTransactionById(int transactionID)
+        {
+            return _context.Transactions.Include(t => t.transaction).FirstOrDefault(t => t.Id == transactionID);
+        }
+        public void AddTransaction(Transaction transaction)
+        {
+            _context.Transactions.Add(transaction);
+            _context.SaveChanges();
+        }
+
+        public void UpdaTransaction(Transaction transaction)
+        {
+            _context.Transactions.Update(transaction);
+            _context.SaveChanges();
+        }
+
+        public void DeleteTransaction(int transactionId)
+        {
+            var transaction = _context.Transactions.Find(transactionId);
+            if (transaction != null)
+            {
+                _context.Transactions.Remove(transaction);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
