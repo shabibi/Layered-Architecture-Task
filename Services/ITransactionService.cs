57b8f82655831eb1705f07dc8b0696777dfe3128
Afra Alshabibi <a.shabibi20@gmail.com>
2024-11-25 15:17:02 +0400

    add Transaction service

---
+++ Services/ITransactionService.cs
@@ -0,0 +1,13 @@
+﻿using ServicesLab1.Models;
+
+namespace ServicesLab1.Services
+{
+    public interface ITransactionService
+    {
+        void AddTransaction(Transaction transaction);
+        void DeleteTransaction(int transactionId);
+        IEnumerable<Transaction> GetAllTransactions();
+        Transaction GetTransactionById(int transactionID);
+        void UpdaTransaction(Transaction transaction);
+    }
+}
---
+++ Services/TransactionService.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ServicesLab1.Repositories;
+using ServicesLab1.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesLab1.Services
+{
+    public class TransactionService : ITransactionService
+    {
+        private readonly ITransactionRepository _transactionRepository;
+
+        public TransactionService(ITransactionRepository transactionRepository)
+        {
+            _transactionRepository = transactionRepository;
+        }
+
+        public IEnumerable<Transaction> GetAllTransactions()
+        {
+            return _transactionRepository.GetAllTransactions();
+        }
+
+        public Transaction GetTransactionById(int transactionID)
+        {
+            return _transactionRepository.GetTransactionById(transactionID);
+        }
+        public void AddTransaction(Transaction transaction)
+        {
+            _transactionRepository.AddTransaction(transaction);
+        }
+        public void UpdaTransaction(Transaction transaction)
+        {
+            _transactionRepository.UpdaTransaction(transaction);
+        }
+        public void DeleteTransaction(int transactionId)
+        {
+            _transactionRepository.DeleteTransaction(transactionId);
+
+        }
+
+    }
+
+}
